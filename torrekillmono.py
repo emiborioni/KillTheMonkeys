cf7993dda85873825e0b11448b062173f2106496
emiborionicab <emiborionicab@gmail.com>
2015-10-14 14:10:42 -0300

    Archivo actualizado a la versión final. Todas las funciones creadas.

--- torrekillmono.py
+++ torrekillmono.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
+#Probando github
 import pilasengine
 import random
 
@@ -10,7 +11,7 @@ pilas = pilasengine.iniciar()
 # Usar un fondo estándar
 pilas.fondos.Pasto()
 # Añadir un marcador
-puntos = pilas.actores.Puntaje(x=230, y=200, color=pilas.colores.blanco)
+puntos = pilas.actores.Puntaje(x=230, y=200, color=pilas.colores.rojo)
 puntos.magnitud = 40
 # Añadir el conmutador de Sonido
 pilas.actores.Sonido()
@@ -20,16 +21,39 @@ balas_simples = pilas.actores.Bala
 monos = []
 
 # Funciones
-def mono_destruido():
-    pass
+#Elimina el actor Mono cuando una bala impacta en él
+def mono_destruido(bala, enemigo):
+    enemigo.eliminar()
+    bala.eliminar()
+    
+    a = monos.index(enemigo)
+    del monos[a]
 
+    puntos.escala=[1, 0, 5, 1], .1
+    puntos.aumentar(1)
 
+#Finaliza el juego cuando un actor Mono impacta en la torreta
+def perder(torreta, enemigo):
+    global fin_de_juego
+    torreta.eliminar()
+    enemigo.sonreir()
+    texto1=pilas.actores.Texto("GAME OVER")
+    texto1.y= 100
+    texto2=pilas.actores.Texto("Perdiste conseguiste %d puntos" % (puntos.obtener()))
+    texto2.y= -100
+    fin_de_juego=True
+
+#Crea nuevos enemigos y les da sus características
 def crear_mono():
+    global fin_de_juego
+    efecto=random.uniform(.25,.75)
     # Crear un enemigo nuevo
     enemigo = pilas.actores.Mono()
     # Hacer que se aparición sea con un efecto bonito
+    abc=random.uniform(0.25, 0.75)
     ##la escala varíe entre 0,25 y 0,75 (Ojo con el radio de colisión)
-    enemigo.escala = .5
+    enemigo.escala=(1,efecto),1
+    enemigo.radio_de_colision= efecto*50
     # Dotarle de la habilidad de que explote al ser alcanzado por un disparo
     enemigo.aprender(pilas.habilidades.PuedeExplotar)
     # Situarlo en una posición al azar, no demasiado cerca del jugador
@@ -68,11 +92,12 @@ def crear_mono():
 
 
 # Añadir la torreta del jugador
-
 torreta = pilas.actores.Torreta(enemigos=monos, cuando_elimina_enemigo=mono_destruido)
 
-pilas.tareas.agregar(1, crear_mono)
-#pilas.mundo.agregar_tarea(1, crear_mono) <-- sintaxis vieja
+crear = pilas.tareas.agregar(1, crear_mono)
+
+#Colision de los enemigos con la torreta
+pilas.colisiones.agregar(torreta, monos, perder)
 
 
 # Arrancar el juego
